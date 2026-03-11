f54c537bc9a53e8b0f6692134b0a3f03f0fe0d01
unknown <lsmxy4@gmail.com>
2026-03-11 19:43:08 +0900

    3-11.2

--- src/main/java/com/example/demo/dto/CreateMemberRequest.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dto;
-
-public record CreateMemberRequest (
-        String name,
-        String email
-){
-}
--- src/main/java/com/example/demo/dto/UpdateMemberRequest.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dto;
-
-public record UpdateMemberRequest (
-        String name,
-        String email
-){
-}
